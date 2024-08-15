2529e5e525c01e3c3df0984ccb8da0546be5493d
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-15 16:50:55 +0530

    second commit

--- 01/script.js
+++ 01/script.js
@@ -1,23 +1,50 @@
 let reduxState = {
-    post: 0,
-    name: "John",
-    age: 30,
+  post: 0,
+  name: "John",
+  age: 30,
+};
+console.log(reduxState);
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "post/INCREMENT":
+      return { ...state, post: state.post + 1 };
+    case "post/DECREMENT":
+      return { ...state, post: state.post - 1 };
+    case "post/INCREMENTby":
+      return { ...state, post: state.post + action.payload };
+    default:
+      return state;
+  }
 }
-console.log(state);
+reduxState = reducer(reduxState, { type: "post/INCREMENT" });
+console.log(reduxState);
 
-let prevstate = state;
+reduxState = reducer(reduxState, { type: "post/DECREMENT" });
+console.log(reduxState);
 
+reduxState = reducer(reduxState, { type: "post/INCREMENT" });
+console.log(reduxState);
 
+reduxState = reducer(reduxState, { type: "hello" });
+console.log(reduxState);
+
+console.log(" ");
+
+reduxState = reducer(reduxState, { type: "post/INCREMENTby", payload: 10 });
+console.log(reduxState);
+
+// let prevstate = state;
 // const increment = () => {
 //     //mutation state ..................
 //     // state.count = state.count + 1;
-//                      // in this state the value of prevstate === state is true 
+//                      // in this state the value of prevstate === state is true
 
 //     // not mutation state ..............
 //     state = { ...state, count: state.count + 1  };
-//                                      // in this state the value of prevstate === state is false 
+//                                      // in this state the value of prevstate === state is false
 
-// } 
+// }
 // increment();
 // console.log(state);
 // console.log(prevstate === state);
@@ -26,4 +53,3 @@ let prevstate = state;
 // setInterval(() => {
 //     increment();
 // },100)
-
